9f790fe5042f8ec8960530e4964f9a4e9ad6eccf
Majoturitich <mturitich@gmail.com>
2025-03-01 21:17:51 -0700

    New changes with styles photos

---
+++ index.js
@@ -0,0 +1,8 @@
+const greeting = document.getElementById("greeting");
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "30px";
+// const username = window.prompt ("what is your name?"," Maria");
+greeting.textContent = "Welcome to my website" "!";
+
+// const myName = "Luci";
+// console.log(myName);
